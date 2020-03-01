d0230def590ef8c4ee4f5578ec69edb61df5b776
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-03-01 14:18:39 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,11 @@
 class Transfer
   # your code here
+  attr_accessor
+  attr_reader :sender, :receiver
+  def initialize (sender, receiver, amount = 0)
+    @sender = sender
+    @receiver = receiver
+    @status = pending
+    @amount = amount
+  end
 end
