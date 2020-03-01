c24fa90660329bf59893f1d692a326f21d690f54
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-03-01 13:46:27 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,8 +3,25 @@ class BankAccount
   def initalize (name, balance = 1000)
     @name = name
     @balance = balance
-    @status = open
+    @status = "open"
   end
 
+  def deposit(amount)
+    @balance += amount
+  end
+  def display_balance
+    @balance
+  end
+  def valid?
+    if @status = "open" || @balance > 0
+      puts "The account is valid."
+    else
+      puts " The account is invalid."
+    end
+  end
+  def close_accout
+    @balance = 0
+    @status = closed
+  end
 
 end
