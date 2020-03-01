1ea49df3b3ca0de0adec3a3838e68cfee2a9d6a9
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-03-01 15:00:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
   def execute_transaction
     while @status == "pending" do
-      if @sender.balance < @amount && !self.valid? 
+      if @sender.balance < @amount && !self.valid?
         @status = "rejected"
         puts "Transaction rejected. Please check your account balance."
     elsif status = "pending"
