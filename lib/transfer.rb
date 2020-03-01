ad02f6d72667dfd43644ab43eeef6e78c67d0931
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-03-01 14:39:17 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
     if (self.valid? && @sender.balance > @amount && @status = "pending")
         @sender.balance -= @amount
         @receiver.balance += @amount
-        @status = "complete"
+        self.status = "complete"
     end
   end
 
